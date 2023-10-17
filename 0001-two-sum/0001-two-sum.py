88f02f024caa39a7884fc174a0e5ae342b4f9a4d
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-10-17 13:09:59 +0530

    Time: 71 ms (48.35%), Space: 17.5 MB (42.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    res.append(i)
-                    res.append(j)
-                    return res
+        res = {}
+
+        size = len(nums)
+
+        for i in range(size):
+            complement = target - nums[i]
+            if complement in res:
+                return [res[complement], i]
+            
+            # if complement does not exist, add the cuurent element nums[i] to res with its index as the value
+            res[nums[i]] = i
+
+        # if not found
+        return []
 
 
         
