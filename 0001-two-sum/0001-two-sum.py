5f6192d772fa636ba59b3cdbb2c797bdc427dab8
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-10-22 11:54:57 +0530

    Time: 73 ms (45.47%), Space: 17.6 MB (22.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # we need to create a hashmap
-        res = {}
+        # creating a hashset to store the unique values from nums
+        nums_hs = {}
 
-        # iterate through all the values of nums
-        for idx in range(len(nums)):
-            complement = target - nums[idx]
-            if complement in res:
-                # return [complement_index_fromhashmap, current_index]
-                return [res[complement], idx]
+        # iterating over nums.
+        for number_idx in range(len(nums)):
+            complement = target - nums[number_idx]
+            # check whether this complement exists in the nums_hs
+            if complement in nums_hs:
+                return [nums_hs[complement], number_idx]
 
+            # if complement does not exist in the nums_hs, add its index to the nums_hs
+            nums_hs[nums[number_idx]] = number_idx
 
-            # add values to the hashmap
-            res[nums[idx]] = idx
-
-        # if not found
+        
+        # at last return empty list if not found
         return []
+
         
