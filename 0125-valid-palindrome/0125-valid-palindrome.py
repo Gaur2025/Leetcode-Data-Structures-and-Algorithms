d2316d6c583c520b9876652760d387d7c198ccc8
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-10-22 10:54:05 +0530

    Time: 44 ms (84.23%), Space: 17 MB (92.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,34 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Approach 1. (TC = O(n), SC = O(n))
+        # using python methods
+        newStr = ""
 
-        # create a valid_str list with lowercase and alphanumeric values only.
-        temp = []
         for char in s:
-            if (ord(char) >= ord("A")) and (ord(char) <= ord("Z")):
-                temp.append(char.lower())
-            elif (ord(char) >= ord("a")) and (ord(char) <= ord("z")):
-                temp.append(char)
-            elif (ord(char) >= ord("0")) and (ord(char) <= ord("9")):
-                temp.append(char)
-            else:    # for non-alphanumeric
-                continue
+            if char.isalnum():
+                newStr += char.lower()
 
-        # till this point we have a list containing all the lower case alphanumeric values from str s.
-
-        # creating 2 pointers --> left and right to check whether left and right character are same.
-        left = 0
-        right = len(temp) - 1
-
-        while left < right:
-            if (temp[left] != temp[right]):
-                return False
-            else:
-                left += 1
-                right -= 1
-        
-        # if all goes fine. Then given input is palindrome
-        return True
-
-
+        # check whether reverse is same as original
+        return (newStr == newStr[::-1])
         
