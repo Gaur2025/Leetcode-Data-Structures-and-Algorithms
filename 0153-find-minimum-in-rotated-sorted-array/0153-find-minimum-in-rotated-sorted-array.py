479002f18933aa01331313c167c7290f0ae92457
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-10-23 22:47:01 +0530

    Time: 30 ms (6.44%), Space: 13.2 MB (97.24%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # try to find th evalue in the sorted half
+        ans = 99999
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if (nums[left] <= nums[mid]):
+                # it means left half is sorted. 
+                ans = min(ans, nums[left])
+                # now move to the other half
+                left = mid + 1
+            else:
+                # right half is sorted
+                ans = min(ans, nums[mid])
+                # move to the other half
+                right = mid - 1
+
+        return ans
+        
