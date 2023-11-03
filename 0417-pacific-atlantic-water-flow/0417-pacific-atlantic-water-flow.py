a34d33d4958ff63715c019edd7ed84aafdc2ee16
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-11-03 19:02:07 +0530

    Time: 240 ms (67.59%), Space: 17.8 MB (83.06%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,51 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        # we need 2 different sets to capture the cells to which water can flow
+        pacific = set()
+        atlantic = set()
+        
+        # defining our rows and cols
+        nrows = len(heights)
+        ncols = len(heights[0])
 
-        def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit
-                or r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
-            ):
+        # water will flow towards the cell if value of cell is greater or equal than preHeight
+        # this is an dfs function
+        def waterFlow(r, c, visited, prevHeight):
+            
+            # base case (flow condition + out of bound rules + already visited)
+            if (r < 0 or r == nrows or c < 0 or c == ncols or heights[r][c] < prevHeight or (r, c) in visited):
                 return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+            visited.add((r, c))
+            # apply dfs in each adjacent direction
+            waterFlow(r + 1, c, visited, heights[r][c])
+            waterFlow(r - 1, c, visited, heights[r][c])
+            waterFlow(r, c + 1, visited, heights[r][c])
+            waterFlow(r, c - 1, visited, heights[r][c])
+        
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+        # row boundary
+        for row in range(nrows):
+            # towards the pacific ocean
+            waterFlow(row, 0, pacific, heights[row][0])
+            # towards the atlantic ocean
+            waterFlow(row, ncols - 1, atlantic, heights[row][ncols - 1])
+
+
+        # col boundary
+        for col in range(ncols):
+            # towards the pacific ocean
+            waterFlow(0, col, pacific, heights[0][col])
+            # towards the atlantic ocean
+            waterFlow(nrows - 1, col, atlantic, heights[nrows - 1][col])    
+
 
         res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
+        for row in range(nrows):
+            for col in range(ncols):
+                if (row, col) in pacific and (row, col) in atlantic:
+                    res.append([row, col])
+
         return res
+
+        
