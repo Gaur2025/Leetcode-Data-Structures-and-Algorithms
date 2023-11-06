abc0efbc9457344a35c9b6d318256d73de7b1ad1
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-11-07 00:00:54 +0530

    Time: 56 ms (45.67%), Space: 18.3 MB (45.29%) - LeetHub

--- 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.py
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.py
@@ -10,27 +10,28 @@ class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
 
         res = []
+
+        # creating a queue
         q = collections.deque()
 
+        # adding root into the queue
         q.append(root)
 
         while q:
             lenQ = len(q)
             level = []
-            
+
+            # iterating over q to get all the level nodes
             for i in range(lenQ):
                 node = q.popleft()
                 if node:
                     level.append(node.val)
-                    # adding all children to the level
+                    # adding node's children to the q
                     for child in node.children:
                         q.append(child)
-                    
+
             if level:
                 res.append(level)
-                    
+
         return res
-         
-
-
         
