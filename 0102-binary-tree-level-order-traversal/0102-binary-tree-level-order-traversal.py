6337c8f59792022f9601c92496988b709de03226
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-11-07 00:14:35 +0530

    Time: 48 ms (35.4%), Space: 16.9 MB (91.96%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -5,37 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Approach 1: BFS (FIFO)
-    # a. Create a queue to store level. Create a res list to store the final result.
-    # b. Insert root into this queue.
-    # c. Now we need to keep inserting children of each node and maintain specific lists for each level.
-    # d. Append each level list to the result list.
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
+
         res = []
-        q = collections.deque()   # as we need to perform FIFO operations
-        # add root to the queue.
+        q = collections.deque()
+
+        # adding root to the queue
         q.append(root)
 
         while q:
             lenQ = len(q)
             level = []
 
+            # for each node in q
             for i in range(lenQ):
-                # pop left elements from the queue and add their respective levels.
                 node = q.popleft()
                 if node:
                     level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
+                    # adding children of node to q
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
 
             if level:
                 res.append(level)
 
         return res
-
-
-    # TC = O(n), n = number of nodes in the given BT
-    # SC = O(n) (Recursive Stack)
-
         
