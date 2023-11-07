b7f7b5788aef059bd47f158e32047676a839734d
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-11-07 22:57:17 +0530

    Time: 59 ms (68.87%), Space: 16.5 MB (36.47%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,11 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # [10, 15, 20] 0
+        # we need to reach at position 0
+        # so we can start getting minimum cost from end of the list
+        cost_copy = cost.copy()
 
-        costCopy = cost.copy()
+        cost_copy.append(0)
 
-        for i in range(len(costCopy) - 3, -1, -1):
-            costCopy[i] += min(costCopy[i + 1], costCopy[i + 2])
+        # we want to start from the 2nd last element of the list in reverse order
+        for i in range(len(cost_copy) - 3, -1, -1):
+            # cost_copy[i] = min(cost_copy[i] + cost_copy[i + 1], cost_copy[i] + cost_copy[i + 2])
+            cost_copy[i] += min(cost_copy[i + 1], cost_copy[i + 2])
 
-        return min(costCopy[0], costCopy[1])
+        # at last we return the minimum of first and second value from the list coz we know we have atleast 2 elements in the list
+        return min(cost_copy[0], cost_copy[1])
         
