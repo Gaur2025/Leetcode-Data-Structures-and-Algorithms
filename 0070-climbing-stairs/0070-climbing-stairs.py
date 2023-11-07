f3dd278cb1e3c7aec95022c8e5b9b527d96b5e8d
Himanshu Gaur <61372212+Gaur2025@users.noreply.github.com>
2023-11-07 22:51:44 +0530

    Time: 36 ms (70.39%), Space: 16.2 MB (36.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # using DP
+        # 0 [1, 2, 3]
+
+        # we can have 2 variables to last and second last element (these both will be 1)
         one, two = 1, 1
 
         for i in range(n - 1):
+            # one will come one place back, and two will go to one's place
             temp = one
             one = one + two
             two = temp
 
+        # at last we return one
         return one
